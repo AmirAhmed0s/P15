b49d5a66a307809b61bd0c05b9766a524f9e1597
AmirAhmed0s <163478540+AmirAhmed0s@users.noreply.github.com>
2024-11-06 19:24:02 +0200

    Update dox_theme_desk.bundle.js

--- dox_theme/public/js/dox_theme_desk.bundle.js
+++ dox_theme/public/js/dox_theme_desk.bundle.js
@@ -1,42 +1,7 @@
 import "./ui/toolbar/navbar.html";
-// TaxesAndTotalsExtend is just variable name
-// erpnext.taxes_and_totals is function contain function you want to override
 
-// const TaxesAndTotalsExtend = frappe.workspace.extend({
-// });
-// console.log("hello from dox")
-// frappe.provide("frappe.view.Workspace");
-// frappe.views.Workspace.extend({
-//     sidebar_item_container(item) {
-//         console.log('Hello from extend taxes and total extend !!');
-//     }
-// });
-
-
-class Custom_Workspace extends frappe.views.Workspace {
-    // sidebar_item_container(item) {
-    //     return $(`
-	// 		<div class="sidebar-item-container ${item.is_editable ? "is-draggable" : ""}" item-parent="${item.parent_page
-    //         }" item-name="${item.title}" item-public="${item.public || 0}">
-	// 			<div class="desk-sidebar-item standard-sidebar-item ${item.selected ? "selected" : ""}">
-	// 				<a
-	// 					href="/app/${item.public
-    //             ? frappe.router.slug(item.title)
-    //             : "private/" + frappe.router.slug(item.title)
-    //         }"
-	// 					class="item-anchor ${item.is_editable ? "" : "block-click"}" title="${__(item.title)}"
-	// 				>
-	// 					<span class="sidebar-item-icon" item-icon=${item.icon || "folder-normal"}>
-    //                     <img class="icon-md mr-2" src=${item.svg} />
-    //                     </span>
-	// 					<span class="sidebar-item-label">${__(item.title)}<span>
-	// 				</a>
-	// 				<div class="sidebar-item-control"></div>
-	// 			</div>
-	// 			<div class="sidebar-child-item nested-container"></div>
-	// 		</div>
-	// 	`);
-    // },
+// Extend the Workspace to customize sidebar items
+class CustomWorkspace extends frappe.views.Workspace {
 	sidebar_item_container(item) {
 		return $(`
 			<div
@@ -53,14 +18,12 @@ class Custom_Workspace extends frappe.views.Workspace {
 								? frappe.router.slug(item.title)
 								: "private/" + frappe.router.slug(item.title)
 						}"
-						class="item-anchor ${item.is_editable ? "" : "block-click"}" title="${__(item.title)}"
+						class="item-anchor ${item.is_editable ? "" : "block-click"}"
+						title="${__(item.title)}"
 					>
-                    
 						<span class="sidebar-item-icon" item-icon=${item.icon || "folder-normal"}>
-                        ${item.svg ? '<img class="icon-md mr-2" src='+item.svg+' />' : frappe.utils.icon(item.icon || "folder-normal","md")}
-                        
-                        
-        </span>
+							${item.svg ? `<img class="icon-md mr-2" src="${item.svg}" />` : frappe.utils.icon(item.icon || "folder-normal","md")}
+						</span>
 						<span class="sidebar-item-label">${__(item.title)}<span>
 					</a>
 					<div class="sidebar-item-control"></div>
@@ -71,8 +34,9 @@ class Custom_Workspace extends frappe.views.Workspace {
 	}
 }
 
+// Wrapper for compatibility with both ERPNext v14 and v15
 frappe.standard_pages["Workspaces"] = function () {
-    var wrapper = frappe.container.add_page("Workspaces");
+    const wrapper = frappe.container.add_page("Workspaces");
 
     frappe.ui.make_app_page({
         parent: wrapper,
@@ -80,8 +44,9 @@ frappe.standard_pages["Workspaces"] = function () {
         title: __("Workspace"),
     });
 
-    frappe.workspace = new Custom_Workspace(wrapper);
-    $(wrapper).bind("show", function () {
+    frappe.workspace = new CustomWorkspace(wrapper);
+
+    $(wrapper).on("show", function () {
         frappe.workspace.show();
     });
 };
